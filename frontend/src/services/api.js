a9cf8f2ce6b3e7453dc9fe98ff73301b03c8f2c3
Punam Channe <punamchanne51@gmail.com>
2026-04-06 12:09:58 +0530

    Update frontend API URL to production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
-
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://parking-management-system-sye4.onrender.com/api';
 
 export const getStats = async () => {
     const response = await axios.get(`${API_BASE_URL}/stats`);
